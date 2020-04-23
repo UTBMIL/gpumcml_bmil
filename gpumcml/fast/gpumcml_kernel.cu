58c4f64568218da95ddf91118487606731864714
Andrew Stier <andrew@cerebri.com>
2020-04-23 15:10:21 -0500

    removed unused findClosest

--- gpumcml/fast/gpumcml_kernel.cu
+++ gpumcml/fast/gpumcml_kernel.cu
@@ -790,46 +790,5 @@ __global__ void sum_A_rz(UINT64 *g_A_rz)
 
 #endif  // _GPUMCML_KERNEL_CU_
 
-__global__ UINT32 findClosest(float arr[],int n, float target) 
-{ 
-  	
-	// Corner cases 
-	if (target <= arr[0]) 
-		return 0; 
-	if (target >= arr[n - 1]) 
-		return n-1; 
 
-	// Doing binary search 
-	int i = 0, j = n, mid = 0; 
-	while (i < j) { 
-		mid = (i + j) / 2; 
-
-		if ((arr[mid] - target) <=0.000001) 
-			return mid; 
-
-		/* If target is less than array element, 
-			then search in left */
-		if (target < arr[mid]) { 
-
-			// If target is greater than previous 
-			// to mid, return closest of two 
-			if (mid > 0 && target > arr[mid - 1]) 
-				return mid; 
-
-			/* Repeat for left half */
-			j = mid; 
-		} 
-
-		// If target is greater than mid 
-		else { 
-			if (mid < n - 1 && target < arr[mid + 1]) 
-				return mid; 
-			// update i 
-			i = mid + 1; 
-		} 
-	} 
-
-	// Only single element left after search 
-	return arr[mid]; 
-} 
 
