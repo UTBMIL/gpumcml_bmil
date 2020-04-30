210aad986c6dd8226a8137aef9c95a14d73793e6
Andrew Stier <andrew@cerebri.com>
2020-04-30 00:03:50 -0500

    corrected plot_spectra for diffuse

--- gpumcml/fast/plot_spectra.m
+++ gpumcml/fast/plot_spectra.m
@@ -1,11 +1,11 @@
-close all; clc
+clear all; close all; clc
 
 musp_vs = 1 * 10; %cm^-1
 gs = [.9];
 
 mua_v = 0.01
 
-% gammas = 0.95;
+
 
 fx = [.01 .025 .05:.05:1.8];
 
@@ -13,7 +13,7 @@ fx = [.01 .025 .05:.05:1.8];
 for g = gs
     for mua_e = mua_v
         for musp_v_cm = musp_vs
-            R_MCM_data = load(['Test/SFDR/SFDR_mu_' num2str(musp_v_cm) '_gamma_' num2str(gam) '_g_' num2str(g) '_mua_' num2str(mua_e) '.mat']);
+            R_MCM_data = load(['Test/SFDR/SFDR_mu_' num2str(musp_v_cm) '_g_' num2str(g) '_mua_' num2str(mua_e) '.mat']);
 
             X = R_MCM_data.SFDR_1Y;
 
