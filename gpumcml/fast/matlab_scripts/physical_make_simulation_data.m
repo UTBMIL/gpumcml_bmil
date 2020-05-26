5e9ae0d986fdb48010f4e45135cb7fe1fe32f8cc
acs3235 <acs3235@node63.bme.utexas.edu>
2020-05-26 16:14:21 -0500

    removed gam greater than 1 + g exclusion

--- gpumcml/fast/matlab_scripts/physical_make_simulation_data.m
+++ gpumcml/fast/matlab_scripts/physical_make_simulation_data.m
@@ -14,10 +14,6 @@ for i = 1:length(gammas)
     gam = gammas(i);
     g = g1(i);
     
-    if gam > 1 + g
-        continue
-    end
-
     musp_v_cm = musp_vs(i);
 
     RunMCw1gamma1g_GK(gam,musp_v_cm,g) 
@@ -31,10 +27,6 @@ for mua_e = 0.01
     for i = 1:length(gammas)
         gam = gammas(i);
         g = g1(i);
-
-        if gam > 1 + g
-            continue
-        end
 
         musp_v_cm = musp_vs(i);
         if isfile(['Test/SFDR/SFDR_mu_' num2str(musp_v_cm) '_gamma_' num2str(gam) '_g_' num2str(g) '_mua_' num2str(mua_e) '.mat'])
