67bf994422c1834d2c8b93ed8800305d45f9e978
acs3235 <acs3235@node63.bme.utexas.edu>
2020-06-26 16:56:57 -0500

    changed test parameters

--- gpumcml/fast/matlab_scripts/make_simulation_data.m
+++ gpumcml/fast/matlab_scripts/make_simulation_data.m
@@ -1,16 +1,16 @@
 clear all; close all; clc
 
 % mua_e = linspace(0.01,5,10);
-mua_e = linspace(0.01,5,5);
+mua_e = linspace(0.01,5,29);
 mua_d = 0;
 thi = 0;
 
-gs = [.1, .3, .5, 0.9];
-% gs = 0.9;
+%gs = [.1, .3, .5, 0.9];
+gs = 0.9;
 
 % musp_vs = linspace(6.5,31.1,10);
 % musp_vs = linspace(35.6,60,41);
-musp_vs = linspace(1.0,6,5) * 10; %cm^-1
+musp_vs = linspace(1.0,6,50) * 10; %cm^-1
 
 
 %%
