9e7fb650e90bfe07fb48ceaabd3ccb91e0ad83de
Andrew Stier <andrew@cerebri.com>
2020-07-21 15:29:34 -0500

    deleted unncessesary comments.

--- gpumcml/fast/matlab_scripts/make_simulation_data.m
+++ gpumcml/fast/matlab_scripts/make_simulation_data.m
@@ -1,106 +1,11 @@
 clear all; close all; clc
 
-
-% gammas = linspace(0.95,1.27,6)
-% musp_vs = linspace(1.9,3.8,6) * 10 %cm^-1
-% gs = [.07, .14, .3]
-% 
-% gammas = linspace(0.95,1.27,20)
-% musp_vs = linspace(1.0,6,50) * 10  %cm^-1
-% gs = [.9]
-
-% gammas = linspace(0.95,1.27,20);
-%gammas = linspace(0.95,1.27,3);
-% gammas = gammas(6:end)
-% gammas = gammas(5);
-%musp_vs = 1*10; %cm^-1
-% musp_vs = musp_vs(44:end);
-
-% %gammas = linspace(0.95,1.27,20);
-% %gammas = gammas(2:end-1);
-% gammas = 1.9;
-% musp_vs = linspace(1.0,35,2) * 10; %cm^-1
-% %musp_vs = musp_vs(2:end-1);
-% %gs = [.07, 0.1, .14, .3];
-% gs = [0.9];
-
-
-mua_es = linspace(0.01,5,15);
-mua_d = 0;
-thi = 0;
-
-musp_vs = linspace(1.0,6,20) * 10;
-gammas = linspace(0.95,2.3,15);
-gs = [0.9];
-
-mua_es = linspace(0.01,5,15);
-% mua_d = 0;
-% thi = 0;
-% 
-musp_vs = linspace(1.0,6,40) * 10;
-gammas = linspace(0.95,2.3,40);
-% 
-
-
-% musp_vs = musp_vs(2:end-1);
-% 
-% gs = [.85 .95];
-
-%%
-% gammas = 0.95;
-% musp_vs = 3*10;
-% gs = .07;
-
-% gammas = [1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9];%[1.65, 1.75, 1.85, 1.95]
-% musp_vs = [0.3, 0.5, 1, 3, 5, 10]*10;%linspace(10,60,5) %cm^-1
-% gs =  [0.75, 0.85, 0.95]; %[.75:.05:0.95]
-
-% gammas = [1.7500,  1.7795, 1.8089, 1.8384, 1.8679, 1.8974];
-
-
-
-
-% gammas = [1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9];
-% musp_vs = [0.5, 1.0510, 1.5408, 2.0306, 3.0102, 3.5000]*10;
-% gs =  [0.9];
-
-% %WAVELENGTH 1
-% n_medium_input = 1.333;
-% n_particle_input = 1.61;
-% lambda = 450;
-% 
-% gammas = [1.03, 1.03, 1.8, 1.8, 2.08, 2.08];
-% musp_vs = [3.6, 5.39, 2.26, 3.4, 2.16, 3.25]*10;
-% gs = [.14, .14, .58, .58, .93, .93];
-% d_particles = [.0878, .0878, .19, .19, 0.99, 0.99]*10^3;
-% d_stds = [.01, .01, .01, .01, .03, .03];
-
-% 
-% %WAVELENGTH 3
-% n_medium_input = 1.333;
-% n_particle_input = 1.59;
-% lambda = 620;
-% 
-% gammas = [.97, .97, 1.24, 1.24, 2.17, 2.17];
-% musp_vs = [1.11, 1.67, 1.54, 2.31, 1.87, 2.80]*10;
-% gs = [.07, .07, .30, .30, .92, .92];
-% d_particles = [.0878, .0878, .19, .19, 0.99, 0.99]*10^3;
-% d_stds = [.01, .01, .01, .01, .03, .03];
-
-% gammas = [1.7500, 1.7795, 1.8089, 1.8384, 1.8679, 1.8974];
-% musp_vs = [0.5, 1.0510, 1.5408, 2.0306, 3.0102, 3.5000]*10;
-% gs = [.9];
-
-%%
-mua_es = 0.082*10;
-musp_vs = 4.1548*10;
-
-
 gs = [0.9];
 
 musp_vs = 3.8809 * 10;
 mua_es = 3.7836;
-gammas = linspace(0.9,1.9,10);
+% gammas = linspace(0.9,1.9,10);
+gammas = 1.9;
 
 for gam = gammas
     for g = gs
@@ -117,9 +22,7 @@ for gam = gammas
 end
 %%
 close all;
-% gammas = linspace(0.95,1.27,20);
 
-% for mua_e = linspace(0.01,5,10);
 for mua_e = mua_es
     for gam = gammas
         for g = gs
