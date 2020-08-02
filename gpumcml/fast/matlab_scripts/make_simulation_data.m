b0e391cfaf2b5690b130085bbd012d21679c2837
Andrew Stier <andrew@cerebri.com>
2020-08-02 13:54:50 -0500

    expanding gamma range, making sparser again

--- gpumcml/fast/matlab_scripts/make_simulation_data.m
+++ gpumcml/fast/matlab_scripts/make_simulation_data.m
@@ -7,13 +7,13 @@ amap = interpolant_data_2.F2;
 
 musp_vs = linspace(5,60,50);
 mua_es = linspace(.01,5,29);
-gammas = linspace(0.95,2,30);
+gammas = union(union(linspace(0.7,0.95,5), linspace(0.95,2,30)),linspace(2,2.2,6));
 
 gs = 0.9;
 
-%musp_vs = musp_vs(1:2:end);
-%mua_es = mua_es(1:2:end);
-%gammas = gammas(1:2:end);
+musp_vs = musp_vs(1:2:end);
+mua_es = mua_es(1:2:end);
+gammas = gammas(1:2:end);
 
 for gamma = gammas
     for g1 = gs
