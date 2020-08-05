5f88c20546e4b34169d234bb4807471252a306ae
acs3235 <acs3235@node63.bme.utexas.edu>
2020-08-05 12:47:38 -0500

    running with higher range

--- gpumcml/fast/matlab_scripts/make_simulation_data.m
+++ gpumcml/fast/matlab_scripts/make_simulation_data.m
@@ -5,23 +5,23 @@ gmap = interpolant_data.F;
 interpolant_data_2 = load('a_interpolant');
 amap = interpolant_data_2.F2;
 
-musp_vs = linspace(5,60,50);
-mua_es = linspace(.01,5,29);
-gammas = [linspace(0.95,2,30) linspace(2,2.2,6)];
+musp_vs = [linspace(5,60,50) linspace(60,100,20)];
+mua_es = [linspace(.01,5,29) 6 7];
+gammas = [linspace(0.7,0.95,6) linspace(0.95,2,30) linspace(2,2.2,6)];
 
 gs = [0.1 0.3 0.5 0.9];
 
-musp_vs = linspace(5,60,5);
-mua_es = linspace(.01,5,7);
-gammas = linspace(0.95,2.2,5);
+%musp_vs = linspace(5,60,5);
+%mua_es = linspace(.01,5,7);
+%gammas = linspace(0.95,2.2,5);
 %gs = 0.9;
 
 %gammas = linspace(2,2.2,6);
 %gammas = linspace(0.7,0.95,6);
 
-%musp_vs = musp_vs(1:2:end);
-%mua_es = mua_es(1:2:end);
-%gammas = gammas(1:2:end);
+musp_vs = musp_vs(1:2:end);
+mua_es = mua_es(1:2:end);
+gammas = gammas(1:2:end);
 
 %gammas = gammas(14);
 
