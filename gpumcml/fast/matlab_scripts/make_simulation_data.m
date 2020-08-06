96d9fccb0433acff69796fd9736c34399151bd1e
acs3235 <acs3235@node63.bme.utexas.edu>
2020-08-06 11:50:43 -0500

    increasing ranges

--- gpumcml/fast/matlab_scripts/make_simulation_data.m
+++ gpumcml/fast/matlab_scripts/make_simulation_data.m
@@ -11,17 +11,9 @@ gammas = [linspace(0.7,0.95,6) linspace(0.95,2,30) linspace(2,2.2,6)];
 
 gs = [0.1 0.3 0.5 0.9];
 
-%musp_vs = linspace(5,60,5);
-%mua_es = linspace(.01,5,7);
-%gammas = linspace(0.95,2.2,5);
-%gs = 0.9;
-
-%gammas = linspace(2,2.2,6);
-%gammas = linspace(0.7,0.95,6);
-
-musp_vs = musp_vs(1:2:end);
-mua_es = mua_es(1:2:end);
-gammas = gammas(1:2:end);
+musp_vs = musp_vs(2:10:end);
+mua_es = mua_es(2:7:end);
+gammas = gammas(2:7:end);
 
 %gammas = gammas(14);
 
